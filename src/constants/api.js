00d4afc422e89275b2ee4b179cae5f65f61429ec
Muthumari-1993 <119999897+Muthumari-1993@users.noreply.github.com>
2024-07-18 20:12:15 +0530

    api

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
     
-//baseURL: 'http://43.228.126.245:4013',
-baseURL: 'http://localhost:5009',
+baseURL: 'http://192.64.114.83:2023',
+// baseURL: 'http://localhost:5009',
 
 
 });
