757b44cd339dc2d88f65dbb19f8faadfec6053ad
Muthumari-1993 <119999897+Muthumari-1993@users.noreply.github.com>
2024-07-23 13:10:58 +0530

    api

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
     
-baseURL: 'https://jewelpro.unitdtechnologies.com:2023',
+baseURL: 'https://jewelpro.unitdtechnologies.com:2024',
 //  baseURL: 'http://localhost:5009',
 
 
